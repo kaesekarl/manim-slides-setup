14df99442e42fb53f4c9e131141b7f8b7abb9c7a
kaesekarl <81841845+kaesekarl@users.noreply.github.com>
2023-09-04 01:42:31 +0200

    Started refactoring on labels. Vertex should be done

--- src/network-creator/Vertex.py
+++ src/network-creator/Vertex.py
@@ -3,17 +3,41 @@ from src.text_elements.Label import Label
 
 
 class VertexLabel(Label):
-    pass
+    def __init__(self, string=None, color=None, font=None, size=None, position=ORIGIN, shift=ORIGIN, tex=True):
+        super().__init__(string, color, font, size, position, shift, tex)
 
 
 class Vertex:
-    def __init__(self, id, color=WHITE, position=ORIGIN):
-        self.id = id
+    def __init__(self, name, color=None, variety=None, position=ORIGIN, scale=1, label_color=None):
+        self.name = name
         self.color = color
-        self.label = VertexLabel()
         self.position = position
+        self.variety = variety
+        self.scale = scale
 
-    def vertex(self, pos: np.ndarray = None):
+        self.vertex_label = VertexLabel()
+        self.label_pos = ORIGIN
+        self.label_color = label_color
+
+
+    def node(self, pos: np.ndarray = None, scale=1):
         if pos is None:
             pos = self.position
-        return None
+        if self.variety is None:
+            raise Exception("Vertex variety not set")
+        if self.color is None:
+            return self.variety.copy().move_to(pos).scale(scale)
+        return self.variety.copy().set_color(self.color).move_to(pos).scale()
+
+    def label(self, string=None, pos: np.ndarray = None, scale=1, color=None):
+        if pos is None:
+            pos = self.label_pos
+        if string is None:
+            string = self.name
+        if color is None:
+            color = self.label_color
+
+        return self.vertex_label.label(string, color=color).copy().shift(pos).scale(scale)
+
+    def vertex(self, pos: np.ndarray = None, scale=1, label_pos=ORIGIN):
+        return VGroup(self.node(ORIGIN, scale), self.label(scale=scale).shift(label_pos)).move_to(pos).copy()
--- src/text_elements/Label.py
+++ src/text_elements/Label.py
@@ -2,4 +2,27 @@ from manim import *
 
 
 class Label:
-    pass
+    def __init__(self, string, color: str = None, font=None, size=None, position=ORIGIN, shift=ORIGIN, tex=False):
+
+        self.string: str = string
+        self.color: str = color
+        self.font: str = font
+        self.size: float = 1
+        self.position: np.ndarray = position
+        self.tex: bool = tex
+
+    def label(self, string, color: str = None):
+        if string is None:
+            string = self.string
+        if string.__len__() == 0:
+            return VGroup()
+        if color is None:
+            color = self.color
+        if self.tex:
+            return Tex(string, color=self.color).move_to(self.position).scale(self.size).set_color(self.color)
+        return Text(string, color=self.color, font=self.font).move_to(self.position).scale(self.size).set_color(self.color)
+
+
+
+
+
